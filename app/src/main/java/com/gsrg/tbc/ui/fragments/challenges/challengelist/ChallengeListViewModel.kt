46d1f3071c8e77f362e798161d57be5da9fdf466
Gabriel Guedes <>
2020-10-19 16:36:36 +0100

    ChallengeRepository.kt is built

--- app/src/main/java/com/gsrg/tbc/ui/fragments/challenges/challengelist/ChallengeListViewModel.kt
+++ app/src/main/java/com/gsrg/tbc/ui/fragments/challenges/challengelist/ChallengeListViewModel.kt
@@ -2,6 +2,9 @@ package com.gsrg.tbc.ui.fragments.challenges.challengelist
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import com.gsrg.tbc.domain.repository.IChallengeRepository
 
 class ChallengeListViewModel
-@ViewModelInject constructor() : ViewModel()
+@ViewModelInject constructor(
+    private val repository: IChallengeRepository
+) : ViewModel()
