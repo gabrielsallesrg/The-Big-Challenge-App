3bd492be7efd658e17ac7c7845e390b2d4bf4eca
Gabriel Guedes <>
2020-10-19 18:41:52 +0100

    View side from the application is working now.

--- app/src/main/java/com/gsrg/tbc/ui/fragments/challenges/challengelist/ChallengeListAdapter.kt
+++ app/src/main/java/com/gsrg/tbc/ui/fragments/challenges/challengelist/ChallengeListAdapter.kt
@@ -2,12 +2,14 @@ package com.gsrg.tbc.ui.fragments.challenges.challengelist
 
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.gsrg.tbc.R
+import com.gsrg.tbc.core.models.Challenge
 
 class ChallengeListAdapter(
-    private val itemClickAction: (Boolean) -> Unit
+    private val itemClickAction: (Challenge) -> Unit
 ) : RecyclerView.Adapter<ChallengeListViewHolder>() {
 
-    private var dataList: MutableList<Boolean> = mutableListOf() //TODO define properly this
+    private var dataList: MutableList<Challenge> = mutableListOf()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChallengeListViewHolder {
         return ChallengeListViewHolder.create(parent)
@@ -15,14 +17,20 @@ class ChallengeListAdapter(
 
     override fun onBindViewHolder(holder: ChallengeListViewHolder, position: Int) {
         dataList[position].let { challenge ->
-            holder.bind("first", "second")
+            val description = holder.itemView.context.getString(
+                R.string.challenge_cell_text,
+                challenge.title,
+                challenge.description
+            )
+
+            holder.bind(description = description)
             holder.itemView.setOnClickListener { itemClickAction.invoke(challenge) }
         }
     }
 
     override fun getItemCount(): Int = dataList.size
 
-    fun submitData(challengeList: List<Boolean>) {
+    fun submitData(challengeList: List<Challenge>) {
         if (challengeList != dataList) {
             dataList = challengeList.toMutableList()
             notifyDataSetChanged()
